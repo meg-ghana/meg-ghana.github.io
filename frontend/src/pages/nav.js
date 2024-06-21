302b7d96cb65e0e602e13cfbce6d6064decdcee2
Meghana Gopannagari <mg76@illinois.edu>
2024-06-20 23:51:10 -0400

    fixed home

--- frontend/src/pages/nav.js
+++ frontend/src/pages/nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import resume from '../MeghanaGopannagari_Resume_sp24.pdf';
+import HomeIcon from '@mui/icons-material/Home';
 const Nav = (props) => {
     return (
         <div className="nav" style={{position: 'fixed', zIndex: 10, width: '100vw', height: '30px', top: 0, left: 0, paddingTop:'20px', margin: '0 auto'}}>
             <header className="nav-header" style={{maxWidth:'800px', paddingLeft: '5vw', paddingRight: '5vw', margin: '0 auto'}}>
-            <a href="/" style = {{float:'left'}}>Meghana Gopannagari</a>&emsp;
+            <a href="/" style = {{float:'left'}}><HomeIcon/></a>&emsp;
                 <div style = {{float:'right'}}>
                 <a href={resume} target="_blank" rel="noopener noreferrer">Resume</a>&emsp;
                 <a href="/education">Education + Skills</a>&emsp;
